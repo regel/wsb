b911a259d7ef939f7fc8c486c31d88d34bd6d9ab
Sebastien Leger <sebastien.regel@gmail.com>
2021-04-02 14:42:48 +0200

    Test HTML table parser

---
+++ finance/html_test.go
@@ -0,0 +1,142 @@
+package finance
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+const sampleHtmlResponse = `
+<html>
+<table>
+  <tr>
+    <td>A</td>
+    <td>B</td>
+  </tr>
+  <tr>
+    <td>C</td>
+    <td>D</td> 
+  </tr>
+</table>
+</html>
+`
+
+const sampleHtmlNoTable = `
+<html>
+</html>
+`
+
+const sampleHtmlEmptyTable = `
+<html>
+<table>
+</table>
+</html>
+`
+
+const sampleTables = `
+<html>
+<table>
+  <tr>
+    <td>One</td>
+  </tr>
+</table>
+<table>
+  <tr>
+    <td>Two</td>
+  </tr>
+</table>
+</html>
+`
+
+func TestHtmlResponse(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var rsp string
+		rsp = sampleHtmlResponse
+		w.Header()["Content-Type"] = []string{"text/html"}
+		fmt.Fprintln(w, rsp)
+	}))
+	defer ts.Close()
+
+	context := context.Background()
+	client := &http.Client{
+		Transport: &http.Transport{},
+		Timeout:   time.Second,
+	}
+
+	tables, err := ReadHtml(context, client, ts.URL)
+	require.Equal(t, io.EOF, err)
+	require.NotNil(t, tables)
+	require.Equal(t, 1, len(tables))
+	require.Equal(t, 2, len(tables[0].Rows))
+}
+
+func TestNoTable(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var rsp string
+		rsp = sampleHtmlNoTable
+		w.Header()["Content-Type"] = []string{"text/html"}
+		fmt.Fprintln(w, rsp)
+	}))
+	defer ts.Close()
+
+	context := context.Background()
+	client := &http.Client{
+		Transport: &http.Transport{},
+		Timeout:   time.Second,
+	}
+
+	tables, err := ReadHtml(context, client, ts.URL)
+	require.Equal(t, io.EOF, err)
+	require.NotNil(t, tables)
+	require.Empty(t, tables)
+}
+
+func TestEmptyTable(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var rsp string
+		rsp = sampleHtmlEmptyTable
+		w.Header()["Content-Type"] = []string{"text/html"}
+		fmt.Fprintln(w, rsp)
+	}))
+	defer ts.Close()
+
+	context := context.Background()
+	client := &http.Client{
+		Transport: &http.Transport{},
+		Timeout:   time.Second,
+	}
+
+	tables, err := ReadHtml(context, client, ts.URL)
+	require.Equal(t, io.EOF, err)
+	require.Equal(t, 1, len(tables))
+	require.Equal(t, 0, len(tables[0].Rows))
+}
+
+func TestTables(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var rsp string
+		rsp = sampleTables
+		w.Header()["Content-Type"] = []string{"text/html"}
+		fmt.Fprintln(w, rsp)
+	}))
+	defer ts.Close()
+
+	context := context.Background()
+	client := &http.Client{
+		Transport: &http.Transport{},
+		Timeout:   time.Second,
+	}
+
+	tables, err := ReadHtml(context, client, ts.URL)
+	require.Equal(t, io.EOF, err)
+	require.NotNil(t, tables)
+	require.Equal(t, 2, len(tables))
+	require.Equal(t, 1, len(tables[0].Rows))
+	require.Equal(t, 1, len(tables[1].Rows))
+}
